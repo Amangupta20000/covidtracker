66d5b5c167be5f576461f3b6868f1572f3965555
Aman Gupta <rajeshgupta@MacBook-Air.local>
2021-10-16 18:10:00 +0530

    my covid app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import About from './Components/About';
@@ -15,7 +15,8 @@ function App() {
             <Header/>
             <NavBar/>
             <Switch>
-                <Route exact path='/' component={Home} />
+            <Route path='/covidtracker' exact component={Home}/>
+            <Route path='/' exact  ><Redirect to='/covidtracker'/> </Route>
                 <Route exact path='/about' component={About} />
                 <Route exact path='/symptoms' component={Symptoms} />
                 <Route exact path='/statewise' component={Statewise} />
