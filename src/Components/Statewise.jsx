b9bfbb393d426efe09bdd3e457d54cba5d92a38e
Aman Gupta <rajeshgupta@MacBook-Air.local>
2022-01-15 15:00:29 +0530

    my covid app

--- src/Components/Statewise.jsx
+++ src/Components/Statewise.jsx
@@ -4,16 +4,28 @@ import moment from 'moment';
 function Statewise() {
     const [data,setData]= useState([]);
     const [time, settime] = useState('');
-    const getCovidData = async () => {
-        try{
-             const res = await fetch('https://api.apify.com/v2/key-value-stores/toDWvRj1JpTXiM8FF/records/LATEST?disableRedirect=true')
-             const actualData = await res.json();
-            setData(actualData.regionData)
-            settime(moment(actualData.lastUpdatedAtApify).format(' h:mm:ss A'))
-        }catch(err){
-             console.log(err);
-        }
-     }
+    // const getCovidData = async () => {
+    //     try{
+    //          const res = await fetch('https://api.apify.com/v2/key-value-stores/toDWvRj1JpTXiM8FF/records/LATEST?disableRedirect=true')
+    //          const actualData = await res.json();
+    //         setData(actualData.regionData)
+    //         settime(moment(actualData.lastUpdatedAtApify).format(' h:mm:ss A'))
+    //     }catch(err){
+    //          console.log(err);
+    //     }
+    //  }
+
+const getCovidData = async () => {
+    try{
+         const res = await fetch('https://api.rootnet.in/covid19-in/stats/latest')
+         const actualData = await res.json();
+       setData(actualData.data.regional)
+       settime(moment(actualData.lastOriginUpdate).format(' h:mm:ss A'))
+    }catch(err){
+         console.log(err);
+    }
+ }
+
     useEffect(() => {
        getCovidData();
     }, [])
@@ -32,10 +44,10 @@ function Statewise() {
                                 <th className='table_heading'>State</th>
                                 <th className='table_heading'>Confirmed</th>
                                 <th className='table_heading'>Active</th>
-                                <th className='table_heading d-sm-block d-none'>New Active</th>
-                                <th className='table_heading'>Recovered</th>
-                                <th className='table_heading'>New Recovered</th>
-                                <th className='table_heading'>Deceased</th>
+                                { // <th className='table_heading d-sm-block d-none'>New Active</th>
+                                }<th className='table_heading'>Recovered</th>
+                               {// <th className='table_heading'>New Recovered</th>
+                                } <th className='table_heading'>Deceased</th>
                             </tr>
                         </thead>
                         <tbody className=' mb-5 text-white scrollResponsive'>
@@ -43,16 +55,16 @@ function Statewise() {
                                 data.map((curElem, ind) =>{
                                     return(
                                         <tr key={ind} className='text-info table_text'>
-                                        <td className='table_text'>{curElem.region}</td>
-                                        <td className='table_text'>{curElem.totalInfected}</td>
-                                        <td className='table_text'>{curElem.activeCases}</td>
-                                        <td className='table_text d-sm-block d-none'>⬆ {curElem.newRecovered+curElem.newInfected}</td>
-                                        <td className='table_text'>{curElem.recovered}</td>
-                                        <td className='table_text'>⬆ {curElem.newRecovered}</td>
-                                        <td className='table_text'>{curElem.deceased}</td>
+                                        <td className='table_text'>{curElem.loc}</td>
+                                        <td className='table_text'>{curElem.confirmedCasesIndian + curElem.confirmedCasesForeign}</td>
+                                        <td className='table_text'>{curElem.totalConfirmed - curElem.discharged - curElem.deaths }</td>
+                                      {//  <td className='table_text d-sm-block d-none'>⬆ {curElem.newRecovered+curElem.newInfected}</td>
+                                    } <td className='table_text'>{curElem.discharged}</td>
+                                      {//  <td className='table_text'>⬆ {curElem.newRecovered}</td>
+                                    } <td className='table_text'>{curElem.deaths}</td>
                                         </tr>
                                     )})
-                            }   
+                                       }   
                         </tbody>
                     </table>
                 </div>
@@ -62,3 +74,6 @@ function Statewise() {
 }
 
 export default Statewise
+
+    
+    
